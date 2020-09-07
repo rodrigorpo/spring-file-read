9da6245a89d25ba8e5a9eafe1fd359bf81a0737b
Rodrigo Pereira <rodrigorpogo@gmail.com>
2020-09-07 19:31:00 -0300

    First commit

---
+++ src/main/java/xyz/rpolnx/spring/file/read/SpringFileReadApplication.java
@@ -0,0 +1,13 @@
+package xyz.rpolnx.spring.file.read;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFileReadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFileReadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/rpolnx/spring/file/read/SpringFileReadApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.rpolnx.spring.file.read;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFileReadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
